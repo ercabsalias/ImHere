72ad6a5d2a9bac1bedb25e8cc97b5402654e3187
ercabsalias <ercabsalias@gmail.com>
2024-04-11 16:57:03 +0100

    Adicionando style paga home

---
+++ src/screens/Home/styles.ts
@@ -0,0 +1,52 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#131016",
+    padding: 24,
+  },
+
+  eventName: {
+    color: "#FDFCFE",
+    fontSize: 24,
+    fontWeight: "bold",
+    marginTop: 48,
+  },
+
+  eventDate: {
+    color: "#6B6B6B",
+    fontSize: 16,
+  },
+
+  input: {
+    flex: 1,
+    marginRight: 12,
+    height: 56,
+    backgroundColor: "#1F1E25",
+    borderRadius: 5,
+    color: "#fff",
+    fontSize: 16,
+    padding: 16,
+  },
+  buttonText: {
+    color: "#fff",
+    fontSize: 25,
+  },
+
+  button: {
+    width: 56,
+    height: 56,
+    borderRadius: 5,
+    backgroundColor: "#31CF67",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+
+  form: {
+    width: "100%",
+    flexDirection: "row",
+    marginTop: 36,
+    marginBottom: 42,
+  },
+});
